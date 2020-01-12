b59540b928715d4d24c0d1b2d54ce00dc8cd9a15
Abinash Kumar Shaw <abinashshaw16@gmail.com>
2020-01-12 18:33:42 +0530

    Changed username and password for our authenticator

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   
-  http_basic_authenticate_with name: "krsna", password: "radhe", except: [:index, :show]
+  http_basic_authenticate_with name: "admin", password: "admin", except: [:index, :show]
 
   def index
     @posts = Post.all
